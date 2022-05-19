fcf31ccc5b977b150378b7de95aaf7ad54a8de15
Rodrigo Bossini <professorbossini@gmail.com>
2022-05-19 19:32:55 -0300

    apostila 07: Listagem 1.3.1
    Método somar implementado e desafio proposto

---
+++ Calculadora.java
@@ -0,0 +1,11 @@
+public class Calculadora{
+    //tipo de retorno: int
+    //nome: somar
+    //lista de parâmetros: (a, b)
+    //corpo
+    int somar (int a, int b){
+        return a + b;
+        //int res = a + b;
+        //return res;    
+    }
+}
---
+++ TesteCalculadora.java
@@ -0,0 +1,12 @@
+public class TesteCalculadora {
+    public static void main(String[] args) {
+        //1. Instanciar a classe Calculadora
+        //Ou seja, construir um objeto do tipo Calculadora
+
+        //2. Obter dois inteiros junto ao usuário
+
+        //3. Somar os dois inteiros utilizando o método somar da calculdora
+
+        //4. Exibir o resultado
+    }
+}
