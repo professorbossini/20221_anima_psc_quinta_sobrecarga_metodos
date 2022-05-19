eac5ccdce5fc4da588094fb68cd5310a43040362
Rodrigo Bossini <professorbossini@gmail.com>
2022-05-19 19:43:20 -0300

    apostila 07: secao 1.4 encerrada

--- TesteCalculadora.java
+++ TesteCalculadora.java
@@ -1,12 +1,22 @@
+import javax.swing.JOptionPane;
+
 public class TesteCalculadora {
     public static void main(String[] args) {
         //1. Instanciar a classe Calculadora
         //Ou seja, construir um objeto do tipo Calculadora
+        //Calculadora calc = new Calculadora();
+        var calc = new Calculadora();
 
         //2. Obter dois inteiros junto ao usuário
+        var a1 = Integer.parseInt(JOptionPane.showInputDialog("Digite o primeiro número"));
+        var a2 = Integer.parseInt(JOptionPane.showInputDialog("Digite o segundo número"));
 
         //3. Somar os dois inteiros utilizando o método somar da calculdora
-
+        var resultado = calc.somar(a1, a2);
+        
         //4. Exibir o resultado
+        JOptionPane.showMessageDialog(null, "Resultado da soma: " + resultado);
+        
+
     }
 }
