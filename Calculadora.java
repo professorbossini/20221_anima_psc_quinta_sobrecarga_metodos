b0d8cb131f4e02d16874e24acd5b69090f21cc3b
Rodrigo Bossini <professorbossini@gmail.com>
2022-05-19 21:01:10 -0300

    apostila 07: fim da apostila

--- Calculadora.java
+++ Calculadora.java
@@ -1,39 +1,77 @@
 public class Calculadora{
-    //tipo de retorno: int
-    //nome: somar
-    //lista de parâmetros: (a, b)
-    //corpo
-    int somar (int a, int b){
-        return a + b;
-        //int res = a + b;
-        //return res;    
-    }
+    //erro em tempo de compilação
+    // int somar (int a, int b){
+    //     return a + b;
+    // }
 
-    float somar(float a, float b){
-        return a + b;
-    }
+    // float somar (int a, int b){
+    //     return a + b;
+    // }
 
-    float somar(int a, float b){
-        return a + b;
-    }
 
-    int somar(int a, int b, int c){
-        return a + b + c;
-    }
-    
-    int somar (String s1, String s2){
-        return Integer.parseInt(s1) + Integer.parseInt(s2);
-    }
-    //impleementar um método somar que viabilize a compilação da linha 19 do outro lado
-    int somar (String s, int a){
-        return Integer.parseInt(s) + a;
-    }
 
-    //e agora?
-    int somar (int a, String s){
-        //reescrever a linha abaixo
-        //não vale usar o operador +
-        return somar (s, a);
-        //return Integer.parseInt(s) + a;
-    }
+    // void multiplicar (int a, int b){
+    //     System.out.println(a * b);
+    // }
+
+    // void multiplicar (int x, int y){
+    //     System.out.println(x * y);
+    // }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// //tipo de retorno: int
+// //nome: somar
+// //lista de parâmetros: (a, b)
+// //corpo
+// int somar (int a, int b){
+//     return a + b;
+//     //int res = a + b;
+//     //return res;    
+// }
+
+// int somar (String s1, String s2){
+//     return Integer.parseInt(s1) + Integer.parseInt(s2);
+// }
+
+// int somar(int a, int b, int c){
+//     return a + b + c;
+// }
+
+// float somar(float a, float b){
+//     return a + b;
+// }
+
+// float somar(int a, float b){
+//     return a + b;
+// }   
+
+
+// int somar (String s, int a){
+//     return Integer.parseInt(s) + a;
+// }
+
+
+// int somar (int a, String s){
+//     return a + Integer.parseInt(s);   
+// }
--- TesteCalculadoraV2.java
+++ TesteCalculadoraV2.java
@@ -1,24 +1,33 @@
 public class TesteCalculadoraV2 {
     public static void main(String[] args) {
         var calc = new Calculadora();
-        int a = 2, b = 3;
-        float f1 = 3.5f, f2 = 4.7F;
+        calc.somar(2, 3);
+        
+        calc.somar(2, 3);
 
-        String s1 = "5", s2 = "74";
-
-        calc.somar(a, b);
-
-        calc.somar(f1, f2);
-
-        calc.somar(a, f1);
-
-        calc.somar(a, b, 15);
-
-        calc.somar(s1, s2);
-
-        calc.somar(s1, b);
-
-        calc.somar(5, "74");
-
+        calc.multiplicar(2, 3);
     }    
 }
+
+
+
+
+// var calc = new Calculadora();
+// int a = 2, b = 3;
+// float f1 = 3.5f, f2 = 4.7F;
+
+// String s1 = "5", s2 = "74";
+
+// calc.somar(a, b);
+
+// calc.somar(f1, f2);
+
+// calc.somar(a, f1);
+
+// calc.somar(a, b, 15);
+
+// calc.somar(s1, s2);
+
+// calc.somar(s1, b);
+
+// calc.somar(5, "74");
